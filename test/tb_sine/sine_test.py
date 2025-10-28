32ae1bae315436bfdbbfcafed27a39f257c66b5b
Evan Li <happyredmapleleaf@gmail.com>
2025-10-27 22:08:24 -0400

    starting sine test

---
+++ test/tb_sine/sine_test.py
@@ -0,0 +1,50 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
+
+HALF_STEP_RATIO = pow(2, 1/12)
+
+subsample_phase = 0
+
+async def inc_subsample_phase(dut, inc):
+    global subsample_phase
+
+    for _ in range(inc):
+        subsample_phase = (subsample_phase + 1) % 256
+        dut.subsample_phase.value = subsample_phase
+        await ClockCycles(dut.clk, 1)
+
+def calculate_step(freq_hz):
+    sample_rate = 28160
+    return round((freq_hz * (2 ** 14)) / sample_rate)
+
+@cocotb.test()
+async def test_project(dut):
+    global subsample_phase
+
+    dut._log.info("Start")
+
+    # approx 7208960 Hz
+    clock = Clock(dut.clk, 139, units="ns")
+    cocotb.start_soon(clock.start())
+
+    # Reset
+    subsample_phase = 0
+    dut._log.info("Reset")
+    dut.subsample_phase.value = 0
+    dut.freq_increment.value = 0
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+    dut._log.info("Reset complete")
+
+    await inc_subsample_phase(dut, 254)
+    
+    dut._log.info("Starting 0 Hz sample")
+    await inc_subsample_phase(dut, 1 + 7)
+    assert dut.out.value == 64, f"Expected 64, got {dut.out.value.integer}"
+    await inc_subsample_phase(dut, 254-7)
+
+    dut._log.info("Starting 1760 Hz tone")
+    dut.freq_increment.value = calculate_step(1760)
+    await inc_subsample_phase(dut, 256*50) # about 2 periods
---
+++ test/tb_sine/tb.v
@@ -0,0 +1,28 @@
+`default_nettype none
+`timescale 1ns / 1ps
+
+module tb ();
+
+  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    #1;
+  end
+
+  // Wire up the inputs and outputs:
+  reg clk;
+  reg rst_n;
+  reg [7:0] subsample_phase;
+  reg [11:0] freq_increment;
+  wire [6:0] out;
+
+  sine sine_inst (
+      subsample_phase,
+      freq_increment,
+      rst_n,
+      clk,
+      out
+  );
+  
+endmodule
