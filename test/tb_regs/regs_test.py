8dd14f72c15f4b4c11dcace1581a01e807d105ae
Evan Li <happyredmapleleaf@gmail.com>
2025-11-09 18:58:09 -0500

    mooore testing

--- test/tb_regs/regs_test.py
+++ test/tb_regs/regs_test.py
@@ -72,4 +72,111 @@ async def test_regs_reset(dut):
     dut._log.info("Reset complete")
 
     await ClockCycles(dut.clk, 1)
-    assert dut.registers_flat.value == 0, f"Expected 0, got {dut.registers_flat.value.integer}"
+    assert dut.registers_flat.value == 0, f"Expected 0, got {dut.registers_flat.value.to_unsigned()}"
+
+@cocotb.test()
+async def test_regs_double_write(dut):
+    await test_setup(dut)
+
+    for reg in range(2):
+        await write_reg(dut, 0x1234, reg)
+        await write_reg(dut, 0x5678, reg)
+
+        await ClockCycles(dut.clk, 1)
+        assert ((dut.registers_flat.value.to_unsigned()) >> (reg * 16)) & 0xFFFF == 0x5678, \
+            f"Expected 0x5678, got {dut.registers_flat.value.to_unsigned()}"
+
+@cocotb.test()
+async def test_regs_timing(dut):
+    await test_setup(dut)
+
+    # 2-stage ff sync for enable and phase so delay must be at least 3 clk cycles
+    for time_delay in [50000, 1000, 100, 10, 4, 3]:
+    # for time_delay in [1000, 100, 10, 4, 3]:
+        dut._log.info(f"Testing with time delay: {time_delay}")
+        for reg in range(2):
+            reg_value = random.randint(0, 0xFFFF)
+            await write_reg(dut, reg_value, reg, delay=time_delay)
+            await ClockCycles(dut.clk, 1)
+            assert ((dut.registers_flat.value.to_unsigned()) >> (reg * 16)) & 0xFFFF == reg_value, \
+                f"Expected {reg_value}, got {dut.registers_flat.value.to_unsigned()}"
+
+@cocotb.test()
+async def test_regs_error_states(dut):
+    for reg in range(2):
+        await test_setup(dut)
+        dut._log.info("Test case: phase = 0, enable rising edge")
+        dut.address.value = reg
+        dut.reg_value.value = 0xFF
+        dut.phase.value = 0
+        dut.enable.value = 0
+        await ClockCycles(dut.clk, 5)
+
+        dut.enable.value = 1
+        await ClockCycles(dut.clk, 5)
+        assert dut.registers_flat.value == 0, "Expected no reg write"
+
+        # reset to state 0
+        dut.enable.value = 0
+        await ClockCycles(dut.clk, 5)
+        assert dut.registers_flat.value == 0, "Expected no reg write"
+
+        await write_reg(dut, 0x1234, reg)
+        assert ((dut.registers_flat.value.to_unsigned()) >> (reg * 16)) & 0xFFFF == 0x1234, \
+            "Expected reg write"
+        
+        await test_setup(dut)
+        dut._log.info("Test case: phase = 1, enable falling edge")
+        dut.address.value = reg
+        dut.reg_value.value = 0xFF
+        dut.phase.value = 1
+        dut.enable.value = 0
+        await ClockCycles(dut.clk, 5)
+
+        dut.enable.value = 1
+        await ClockCycles(dut.clk, 5)
+
+        # should already be reset to state 0
+        dut.enable.value = 0
+        await ClockCycles(dut.clk, 5)
+        assert dut.registers_flat.value == 0, "Expected no reg write"
+
+        await write_reg(dut, 0x1234, reg)
+        assert ((dut.registers_flat.value.to_unsigned()) >> (reg * 16)) & 0xFFFF == 0x1234, \
+            "Expected reg write"
+        
+        await test_setup(dut)
+        dut._log.info("Test case: enabled = 1, phase rising edge")
+        dut.address.value = reg
+        dut.reg_value.value = 0xFF
+        dut.phase.value = 1
+        dut.enable.value = 0
+        await ClockCycles(dut.clk, 5)
+
+        dut.enable.value = 1
+        await ClockCycles(dut.clk, 5)
+
+        dut.phase.value = 0
+        await ClockCycles(dut.clk, 5)
+
+        dut.phase.value = 1
+        await ClockCycles(dut.clk, 5)
+        assert dut.registers_flat.value == 0, "Expected no reg write"
+
+        # reset to state 0
+        dut.enable.value = 0
+        await ClockCycles(dut.clk, 5)
+        assert dut.registers_flat.value == 0, "Expected no reg write"
+
+        await write_reg(dut, 0x1234, reg)
+        assert ((dut.registers_flat.value.to_unsigned()) >> (reg * 16)) & 0xFFFF == 0x1234, \
+            "Expected reg write"
+    
+@cocotb.test()
+async def test_regs_large_addresses(dut):
+    await test_setup(dut)
+
+    for reg in range(2, 15):
+        await write_reg(dut, 0x1234, reg)
+        await ClockCycles(dut.clk, 1)
+        assert dut.registers_flat.value == 0, f"Expected 0, got {dut.registers_flat.value.to_unsigned()}"
